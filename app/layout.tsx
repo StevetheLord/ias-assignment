a35bc1869e4083127eed9c517d6698a2157b7d57
Tanasescu Stefan <stevethelord1@gmail.com>
2024-12-05 15:02:58 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-300`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
 }
+
